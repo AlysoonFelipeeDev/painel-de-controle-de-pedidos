06d7847c479a0ec59d5c76c2ebac72a305fd5425
jean <alysoonfelipe@hotmail.com>
2026-04-08 17:23:54 -0300

    feat: implementa lógica de filtragem de pedidos

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,37 @@
+import { DashboardFilters } from "../components/DashboardFilters";
 import { OrdersTable } from "../components/OrdersTable";
 import { PageHeader, PageShell, PageSubtitle, PageTitle } from "../components/styles/PageShell";
 import { useOrder } from "../hooks/useOrders";
+import { useAppSelector } from "../store/reduxHooks";
 
 export function Dashboard() {
     const {orders, isLoading, isError} = useOrder()
+    const filters = useAppSelector(state => state.filters)
+
+    const today = new Date()
+    const days = filters.period === '7d' ? 7 : filters.period === '30d' ? 30 : 90
+    const dateLimit = new Date()
+    dateLimit.setDate(today.getDate() - days)
+
+    const ordersFiltered = orders.filter(order => {
+
+        if(filters.status !== 'todos' && order.status !== filters.status) {
+            return false
+        }
+        if(filters.searchOrder && 
+            !order.trackingCode.includes(filters.searchOrder) && 
+            !order.customer.toLowerCase().includes(filters.searchOrder.toLowerCase())) {
+                return false
+            }
+        if (new Date(order.createdAt) < dateLimit) {
+            return false  
+        }
+        return true
+    })
+
     if(isLoading) return <div>Carregando...</div>
     if(isError) return <div>Erro ao buscar dados</div>
+    
 
     return (
         <PageShell>
@@ -13,7 +39,8 @@ export function Dashboard() {
                 <PageTitle>Painel de Pedidos</PageTitle>
                 <PageSubtitle>Visão geral de logistica e indicadores operacionais</PageSubtitle>
             </PageHeader>
-            <OrdersTable orders={orders}/>
+            <DashboardFilters/>
+            <OrdersTable orders={ordersFiltered}/>
         </PageShell>
     )
 }

6743eedfa95f2a0bca6b1745b837f3fd97f53fbc
jean <alysoonfelipe@hotmail.com>
2026-04-07 23:58:55 -0300

    chore: configura TanStack Query e centraliza providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import { Dashboard } from './pages/Dashboard'
 import { Order } from './pages/Order'
-import { Provider } from 'react-redux'
-import { store } from './store'
 
 function App() {
   return (
-    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Dashboard/>} />
           <Route path='/pedido/:id' element={<Order/>} />
         </Routes>
       </BrowserRouter>
-    </Provider>
   ) 
 }
 export default App

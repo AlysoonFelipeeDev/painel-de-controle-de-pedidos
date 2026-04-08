b3ae778bbf28c906f56c456e763c83ad98480cd7
jean <alysoonfelipe@hotmail.com>
2026-04-08 14:40:44 -0300

    feat: adiciona listagem de pedidos no Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,34 @@
 import { PageHeader, PageShell, PageSubtitle, PageTitle } from "../components/styles/PageShell";
+import { useOrder } from "../hooks/useOrders";
+import { format } from 'date-fns'
 
 export function Dashboard() {
+    const {orders, isLoading, isError} = useOrder()
+    if(isLoading) return <div>Carregando...</div>
+    if(isError) return <div>Erro ao buscar dados</div>
+
+
+
     return (
         <PageShell>
             <PageHeader>
                 <PageTitle>Painel de Pedidos</PageTitle>
                 <PageSubtitle>Visão geral de logistica e indicadores operacionais</PageSubtitle>
             </PageHeader>
+                <ul>
+                    {orders.map(order => {
+                        const lastUpdate = order.statusHistory.at(-1)
+                        return (
+                            <li key={order.id}>
+                                <p>Pedido: {order.trackingCode}</p>
+                                <p>Cliente: {order.customer}</p>
+                                <p>Status: {lastUpdate.status}</p>
+                                <p>Atualizado em: {format( new Date(lastUpdate.timestamp), "dd/MM/yyyy 'às' HH:mm")}</p>
+                                <p>Total: R$ {order.value}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
         </PageShell>
     )
 }

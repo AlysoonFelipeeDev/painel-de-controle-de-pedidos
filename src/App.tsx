dea8f37cc840905f1c154686aca3ea86c5626a65
jean <alysoonfelipe@hotmail.com>
2026-04-07 23:23:51 -0300

    chore: integra Provider do Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import { Dashboard } from './pages/Dashboard'
 import { Order } from './pages/Order'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Dashboard/>} />
-        <Route path='/pedido/:id' element={<Order/>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>} />
+          <Route path='/pedido/:id' element={<Order/>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   ) 
 }
 export default App

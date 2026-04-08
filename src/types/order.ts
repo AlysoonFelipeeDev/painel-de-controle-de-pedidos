21cc957505ec670298f247f6d88ecce69f6f3d14
jean <alysoonfelipe@hotmail.com>
2026-04-08 17:23:25 -0300

    chore: adiciona tipo OrderStatus

--- src/types/order.ts
+++ src/types/order.ts
@@ -6,6 +6,9 @@ export interface Order {
     value: number,
     createdAt: string,
     estimatedDelivery: string,
-    deliveredAt: string,
-    statusHistory: {status: string, timesTamp: string}[],
+    deliveredAt?: string,
+    statusHistory: {status: string, timestamp: string}[],
 }
+
+export type OrderStatus = 'todos' | 'pendente' | 'processando' | 'em_rota' | 'entregue' | 'cancelado'
+
